002a7cae9c661d2e98da0e5bfd4144d2f3221ad6
Jack Skrable <jskrable@bu.edu>
2018-11-06 16:07:21 -0500

    class edits

--- tsp.py
+++ tsp.py
@@ -1,19 +1,42 @@
 import random
 
-city_list = ['A','B','C','D']
+cities = []
 
-dist_matrix = [[0,54,10,67], # A
-			   [54,0,47,21], # B
-			   [10,47,0,31], # C
-			   [67,21,31,0]] # D
+dist_matrix = {}
 
 def randCoords():
 	return (random.randint(0,50),random.randint(0,50))
 
+def dist(a,b):
+	x = abs(a[0]-b[0])
+	y = abs(a[1]-b[1])
+	return int((x**2 + y**2)**0.5)
+
 class City(object):
+
 	def __init__(self, name, coords):
 		self.name = name
 		self.coords = coords
+
+	def addToList(self, name):
+		cities.append(self.name)
+
+	def popDist(self, coords):
+		n = len(cities)
+		for i in range(n-1):
+			for j in range(i+1,n):
+				dist_matrix[i,j] = dist(i.coords,j.coords)
+				dist_matrix[j,i] = dist_matrix[i,j]
+
+
+
+
+
+def newCity(c):
+
+	c = City(c,randCoords())
+	c.addToList(c)
+	return c
 
 for i, val in enumerate(city_list):
 	val = City(val,randCoords())
