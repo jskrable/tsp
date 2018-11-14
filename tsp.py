738003c9185c3161721b22e433591affc757a6f5
Jack Skrable <jskrable@bu.edu>
2018-11-14 10:41:09 -0500

    v0.1 creates three random cities and displays info

--- tsp.py
+++ tsp.py
@@ -1,44 +1,61 @@
 import random
 
+# init globals
 cities = []
-
 dist_matrix = {}
 
+# function to get random coordinates
 def randCoords():
 	return (random.randint(0,50),random.randint(0,50))
 
+# function to calculate euclidian distance
 def dist(a,b):
 	x = abs(a[0]-b[0])
 	y = abs(a[1]-b[1])
 	return int((x**2 + y**2)**0.5)
 
+# object class to hold a city
 class City(object):
 
+	# constructor
 	def __init__(self, name, coords):
 		self.name = name
 		self.coords = coords
 
-	def addToList(self, name):
-		cities.append(self.name)
+	# add to global city list
+	def addToList(self):
+		cities.append(self)
 
-	def popDist(self, coords):
-		n = len(cities)
-		for i in range(n-1):
-			for j in range(i+1,n):
-				dist_matrix[i,j] = dist(i.coords,j.coords)
-				dist_matrix[j,i] = dist_matrix[i,j]
+# populates global distance matrix
+def popDist():
+	global cities
+	n = len(cities)
+	for i in range(n-1):
+		for j in range(i+1,n):
+			dist_matrix[i,j] = dist(cities[i].coords,cities[j].coords)
+			dist_matrix[j,i] = dist_matrix[i,j]
 
-
-
-
-
+# add new city in random location
 def newCity(c):
-
+	global cities
 	c = City(c,randCoords())
-	c.addToList(c)
+	c.addToList()
+	popDist()
 	return c
 
-for i, val in enumerate(city_list):
-	val = City(val,randCoords())
-	print(val.name, val.coords)
+# show list of cities
+def showCities():
+	global cities 
+	for i, val in enumerate(cities):
+		print(val.name, val.coords)
+
+"""
+main: create three cities and display their info
+"""
+a = newCity('a')
+b = newCity('b')
+c = newCity('c')
+
+showCities()
+
 
