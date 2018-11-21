d4c1e5ae3e869df815a1e9c58f95cdf6d344f248
John Skrable <jskrable@gmail.com>
2018-11-20 19:08:02 -0500

    added formal argument parsing and inital tour display

--- tsp.py
+++ tsp.py
@@ -1,4 +1,4 @@
-import random, sys
+import random, sys, argparse
 from dataclasses import dataclass
 import matplotlib.pyplot as plt
 
@@ -6,6 +6,20 @@ import matplotlib.pyplot as plt
 cities = []
 dist_matrix = {}
 path = []
+
+# function to parse arguments sent to CLI
+def argParser():
+	# setup argument parsing with description and -h method
+	parser = argparse.ArgumentParser(description='Solve the traveling salesman problem using Markov Chain Monte Carlo Method')
+	# add size int
+	parser.add_argument('-s','--size',default=20,type=int,nargs='?', 
+						help='the number of cities to travel')
+	# add iterations int
+	parser.add_argument('-i','--iterations',default=10000,type=int,nargs='?',
+						help='the number of simulations to run')
+	#parse args and return
+	args = parser.parse_args()
+	return args.size, args.iterations
 
 # function to get random coordinates
 def rand(size):
@@ -17,6 +31,15 @@ def dist(a,b):
 	y = abs(a[1]-b[1])
 	return int((x**2 + y**2)**0.5)
 
+# function to calc total tour dist
+def tourDist(tour):
+	d = 0
+	for i in range(len(tour)-1):
+		a = (cities[i].x,cities[i].y)
+		b = (cities[i+1].x,cities[i+1].y)
+		d = d + dist(a,b)
+	return d
+
 # dataclass version
 @dataclass
 class City:
@@ -27,9 +50,8 @@ class City:
 
 	def addToList(self):
 		cities.append(self)
-
+"""
 @dataclass
-
 class Problem:
 
 	initial: bool
@@ -48,7 +70,7 @@ class Node:
 	depth: int
 	parent: object
 	state: object
-
+"""
 
 # populates global distance matrix
 def popDist():
@@ -72,6 +94,7 @@ def newCity(c):
 # show list of cities
 def showCities():
 	global cities 
+	print('Here is a list of the cities and their coordinates: ')
 	for i, val in enumerate(cities):
 		print(val.name, (val.x, val.y))
 
@@ -86,28 +109,44 @@ def plotTSP(cities, complete):
 	if complete:
 		x.append(cities[0].x)
 		y.append(cities[0].y)	
-		plt.plot(x,y,'ro-')
+		plt.plot(x,y,'bo-')
 	else:
 		plt.plot(x,y,'ro')
 	plt.show()
 
+# solve problem using mcmc
+def mcmc(cities,iterations):
+	# get initial tour in order of city name
+	tour = []
+	for city in cities:
+		tour.append(city)
+	# get initial tour distance
+	dist = tourDist(tour)
+	#for i in range(iterations):
+
+
+
 """
 main: create three cities and display their info
 """
-size = int(sys.argv[1])
+args = argParser()
+size = args[0]
+iterations = args[1]
 
 for i in range(size):
 	i = newCity(i)
 
-"""
-a = newCity('a')
-b = newCity('b')
-c = newCity('c')
-"""
+tour = []
+for city in cities:
+	tour.append(city)
+
+print('total tour distance is: ')
+print(tourDist(tour))
 
 showCities()
 popDist()
-print(dist_matrix)
+# print(dist_matrix)
 plotTSP(cities,False)
+plotTSP(tour,True)
 
 
