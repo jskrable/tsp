9465d3b189623a9b7ccbd340ea43b338242add7a
John Skrable <jskrable@gmail.com>
2018-12-10 21:13:08 -0500

    added results plotting function

--- collect_data.py
+++ collect_data.py
@@ -1,7 +1,7 @@
 import subprocess as sub
 
 # script to execute program in sets of 10 with incrementing size
-for i in range(5, 10):
+for i in range(5, 100):
     for j in range(1, 11):
         p = sub.Popen('py tsp.py -s '+str(i), shell=True)
         print('running size ' + str(i) + ' simulation ' + str(j))
--- output.py
+++ output.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pandas as pd
 import logging as log
 import imageio as im
 import matplotlib.pyplot as plt
@@ -60,6 +61,17 @@ def plot_tsp(state):
     # clear plot for next call
     plt.clf()
 
+
+def plot_stats(data_file, output):
+    # Function to plot statistics 
+
+    x = []
+    y = []
+
+    datafile = pd.read_csv('run_stats.txt',sep=',')
+
+    plt.plot(x, y, 'ro')
+    plt.show()
 
 def write_results(filename, output):
     # function to write results to output file
